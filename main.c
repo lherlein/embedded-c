9578f19987da35453f285dc3e1b1bda6c441686f
Luca Herlein <luca@herlein.com>
2024-08-02 13:05:45 -0700

    test push

--- main.c
+++ main.c
@@ -32,17 +32,17 @@ int main() {
     puts("Hello, world!");
 
 
-    gpio_set_function(LED_PIN, GPIO_FUNC_PWM);
-    uint slice_num = pwm_gpio_to_slice_num(LED_PIN);
-    uint chan = pwm_gpio_to_channel(LED_PIN);
-    pwm_set_freq_duty(slice_num, chan, PWM_FREQ, 5);
-    pwm_set_enabled(slice_num, true);
+    // gpio_set_function(LED_PIN, GPIO_FUNC_PWM);
+    // uint slice_num = pwm_gpio_to_slice_num(LED_PIN);
+    // uint chan = pwm_gpio_to_channel(LED_PIN);
+    // pwm_set_freq_duty(slice_num, chan, PWM_FREQ, 5);
+    // pwm_set_enabled(slice_num, true);
 
-    int counter = 0;
-    while (1) {
-        counter = (counter + 1) % 101;
-        pwm_set_freq_duty(slice_num, chan, PWM_FREQ, counter);
-        sleep_ms(50);
-        printf("Counter: %d\n", counter);
-    }
+    // int counter = 0;
+    // while (1) {
+    //     counter = (counter + 1) % 101;
+    //     pwm_set_freq_duty(slice_num, chan, PWM_FREQ, counter);
+    //     sleep_ms(50);
+    //     printf("Counter: %d\n", counter);
+    // }
 }
